6f7e5f557b6bf4c1452d68e761cd76cc222b65c6
maiconio <maiconscosta@gmail.com>
2015-07-12 21:54:23 -0300

    sqlite database creation

--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var router = mux.NewRouter()
+var db *sql.DB
+
+func init() {
+	_, errCheckExists := os.Stat("./blog.db")
+
+	db, err := sql.Open("sqlite3", "./blog.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if os.IsNotExist(errCheckExists) {
+		fmt.Printf("File blog.db not exists. \nCreating initial database.\n")
+
+		sqlCreateDB := `create table post (id integer not null primary key, post text, date datetime);`
+
+		_, err = db.Exec(sqlCreateDB)
+		if err != nil {
+			log.Printf("%q: %s\n", err, sqlCreateDB)
+			return
+		}
+		fmt.Printf("Initial database created.\n")
+	}
+}
 
 func main() {
 	router.HandleFunc("/", indexPageHandler).Methods("GET")
