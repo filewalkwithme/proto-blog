3a58f172428de99df4ee3e3f77938b73c8e92c97
maiconio <maiconscosta@gmail.com>
2015-07-16 23:29:06 -0300

    adding blog.cfg

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 
+	ini "github.com/Unknwon/goconfig"
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -15,8 +16,20 @@ var router = mux.NewRouter()
 
 //DB is the global DB object
 var DB *sql.DB
+var cfg *ini.ConfigFile
 
 func init() {
+	cfg, err := ini.LoadConfigFile("blog.cfg")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	authorName, err := cfg.GetValue("author", "name")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%v \n", authorName)
+
 	_, errCheckExists := os.Stat("./blog.db")
 
 	db, err := sql.Open("sqlite3", "./blog.db")
@@ -28,7 +41,9 @@ func init() {
 	if os.IsNotExist(errCheckExists) {
 		fmt.Printf("File blog.db not exists. \nCreating initial database.\n")
 
-		sqlCreateDB := `create table posts (id integer not null primary key, title text, src_content text, html_content text, date datetime);`
+		sqlCreateDB := `
+		create table posts (id integer not null primary key, title text, src_content text, html_content text, date datetime);
+		`
 
 		_, err = db.Exec(sqlCreateDB)
 		if err != nil {
