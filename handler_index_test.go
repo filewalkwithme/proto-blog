b724bc41add687537e5a0dd52abb1de403c2b892
maiconio <maiconscosta@gmail.com>
2015-09-08 00:41:07 -0300

    testing: index page handler

---
+++ handler_index_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestIndexPageHandler(t *testing.T) {
+	var b blog
+	b.loadConfig("blog.cfg")
+	b.port = "8081"
+	go b.start()
+
+	resp, err := http.Get("http://localhost:" + b.port)
+	if err != nil {
+		t.Fatalf("%v\n", err)
+	}
+
+	if resp.StatusCode != 200 {
+		t.Fatalf("wrong StatusCode: %v (expected: 200)\n", resp.StatusCode)
+	}
+}
